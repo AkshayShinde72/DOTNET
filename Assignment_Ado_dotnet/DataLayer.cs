8d24fb2d89369e0700e85cd089a25dd0fc6768b6
Akshay Shinde <114301760+AkshayShinde72@users.noreply.github.com>
2023-11-10 09:50:33 +0530

    Add files via upload

---
+++ Assignment_Ado_dotnet/DataLayer.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Assignment_Ado_dotnet
+{
+    internal class DataLayer
+    {
+        private string _connectionString;
+        public DataLayer(IConfiguration iconfiguration)
+        {
+            _connectionString = iconfiguration.GetConnectionString("Default");
+        }
+
+        public SqlConnection getconnection()
+        {
+            SqlConnection sqlconn = new SqlConnection();
+            sqlconn.ConnectionString = _connectionString;
+            return sqlconn;
+        }
+
+        public int InsertData(Employee_poco e)
+        {
+            SqlConnection sqlconn = null;
+            SqlCommand sqlcmd;
+            int record = 0;
+
+            try
+            {
+                sqlconn = getconnection();
+                sqlcmd = new SqlCommand("storedata", sqlconn);
+                sqlcmd.CommandType = CommandType.StoredProcedure;
+
+                sqlcmd.Parameters.Add("@pname", SqlDbType.NVarChar).Value = e.Name;
+                sqlcmd.Parameters.Add("@psalary", SqlDbType.Float).Value = e.Salary;
+                sqlcmd.Parameters.Add("@pgender", SqlDbType.Char).Value = e.Gender;
+                sqlcmd.Parameters.Add("@paddress", SqlDbType.NVarChar).Value = e.Address;
+
+                sqlconn.Open();
+                record = sqlcmd.ExecuteNonQuery();
+                Console.WriteLine("Insert Data succesfully");
+            }
+            catch (SqlException se)
+            { Console.WriteLine(se.Message); }
+
+            finally
+            {
+                sqlconn.Close();
+            }
+            return record;
+
+
+        }
+
+        public List<Employee_poco> GetList()
+        {
+            var listEmployee = new List<Employee_poco>();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(_connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetProductsByName", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //cmd.Parameters.Add("@ProductName",SqlDbType.NVarChar).Value=
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        listEmployee.Add(new Employee_poco
+                        {
+                            Id = Convert.ToInt32(rdr["Id"]),
+                            Name = rdr["Name"].ToString(),
+                            Salary = Convert.ToSingle(rdr["Salary"]),
+                            Address = rdr["Address"].ToString(),
+                            Gender = rdr["Gender"].ToString()
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return listEmployee;
+        }
+
+        public void Update_Employee(Employee_poco e, int id)
+        {
+            try
+            {
+                SqlConnection con = getconnection();
+                SqlCommand cmd = new SqlCommand("@Update", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@pid", SqlDbType.Int).Value = e.Id;
+                cmd.Parameters.Add("@pname", SqlDbType.NVarChar).Value = e.Name;
+                cmd.Parameters.Add("@psalary", SqlDbType.Float).Value = e.Salary;
+                cmd.Parameters.Add("@pgender", SqlDbType.Char).Value = e.Gender;
+                cmd.Parameters.Add("@paddress", SqlDbType.NVarChar).Value = e.Address;
+                cmd.Parameters.Add("@PCID", SqlDbType.Int).Value = id;
+                con.Open();
+                Console.WriteLine("Connected");
+                int record = cmd.ExecuteNonQuery();
+                Console.WriteLine("Rows Affected = " + record);
+                con.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                SqlConnection con = getconnection();
+                SqlCommand cmd = new SqlCommand("@Delete", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@pid", SqlDbType.Int).Value = id;
+                con.Open();
+                Console.WriteLine("Connected");
+                int record = cmd.ExecuteNonQuery();
+                Console.WriteLine(" Delete Command executed and Affected Rows = " + record);
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        //_____________________________________________________________________________________________________________
+
+        public Employee_poco search(int id)
+        {
+            SqlConnection con = null;
+            Employee_poco c = null;
+            try
+            {
+              con = getconnection();
+                SqlCommand cmd = new SqlCommand("@Search", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@pid", SqlDbType.Int).Value = id;
+
+                c= new Employee_poco();
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        c = new Employee_poco();
+                        c.Id = Convert.ToInt32(reader["Id"]);
+                        c.Name = Convert.ToString(reader["Name"]);
+                        c.Address = Convert.ToString(reader["Address"]);
+                        c.Salary = Convert.ToSingle(reader["Salary"]);
+                        c.Gender = Convert.ToString(reader["Gender"]);
+
+                        break;
+                    }
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return c;
+        }
+    }
+}
+    
---
+++ Assignment_Ado_dotnet/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using System.Reflection;
+using System.Xml.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace Assignment_Ado_dotnet
+{
+    internal class Program
+    {
+        private static IConfiguration _iconfiguration;
+        static void Main(string[] args)
+        {
+
+            GetAppSettingsFile();
+            //EmpDisplay();
+            //update();
+            //Delete();
+            search();
+
+        }
+
+        static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("Pro.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+
+        }
+
+        static void EmpDisplay()
+        {
+
+
+
+            Employee_poco p1 = new Employee_poco { Name = "Shubham", Salary = 1000000, Gender = "Male", Address = "Ch.Sambhajinagar", };
+            DataLayer obj = new DataLayer(_iconfiguration);
+            int a = obj.InsertData(p1);
+
+
+
+
+            Console.WriteLine("----------------------------");
+
+            List<Employee_poco> ls = obj.GetList();
+            foreach (var x in ls)
+                Console.WriteLine("{0}  {1}  {2}  {3} {4}", x.Id, x.Name, x.Salary, x.Gender, x.Address);
+            ;
+            Console.ReadLine();
+
+        }
+
+        static void update()
+        {
+            DataLayer obj = new DataLayer(_iconfiguration);
+            obj.Update_Employee(new Employee_poco { Name = "Amruta", Salary = 200, Gender = "Female", Address = "Bhiloli" }, 6);
+
+            Console.WriteLine("----------------------------");
+
+            List<Employee_poco> ls = obj.GetList();
+            foreach (var x in ls)
+                Console.WriteLine("{0}  {1}  {2}  {3} {4}", x.Id, x.Name, x.Salary, x.Gender, x.Address);
+            ;
+            Console.ReadLine();
+        }
+
+        static void Delete()
+        {
+            DataLayer obj = new DataLayer(_iconfiguration);
+            obj.Delete(1005 );
+
+            List<Employee_poco> ls = obj.GetList();
+            foreach (var x in ls)
+                Console.WriteLine("{0}  {1}  {2}  {3} {4}", x.Id, x.Name, x.Salary, x.Gender, x.Address);
+            ;
+            Console.ReadLine();
+        }
+
+        static void search()
+        {
+            DataLayer obj = new DataLayer(_iconfiguration);
+
+           Employee_poco s = obj.search(4);
+            Console.WriteLine("{0} {1} {2} {3} {4}",s.Id, s.Name,s.Salary, s.Gender, s.Address);
+
+        }
+    }
+}
