23c1e69f370ee0bdb523880939a38b163b6615d5
Akshay Shinde <114301760+AkshayShinde72@users.noreply.github.com>
2023-11-10 09:46:39 +0530

    Add files via upload

---
+++ Assignment_day12_1/company_adiodotnet/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace company_adiodotnet
+{
+    internal class Productlayer
+    {
+
+        private string _connectionString;
+        public Productlayer(IConfiguration iconfiguration)
+        {
+            _connectionString = iconfiguration.GetConnectionString("Default");
+        }
+
+        public void Products()
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Employee", con);
+                con.Open();
+                Console.WriteLine("connected");
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2}", rdr["Id"], rdr["Name"], rdr["Salary"]);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Assignment_day12_1/company_adiodotnet/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace company_adiodotnet
+{
+    public class Program
+    {
+        private static IConfiguration _iconfiguration;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            GetAppsettingsFile();
+            PrintProduct();
+         }
+        static void GetAppsettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+
+        }
+
+        static void PrintProduct()
+        {
+            Productlayer obj = new Productlayer(_iconfiguration);
+            obj.Products();
+        }
+       
+
+
+    }
+}
