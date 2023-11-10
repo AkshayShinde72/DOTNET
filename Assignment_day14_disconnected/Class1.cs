91b2a0705dace5de539efa3f8fb544a61fda149b
Akshay Shinde <114301760+AkshayShinde72@users.noreply.github.com>
2023-11-10 09:52:24 +0530

    Add files via upload

---
+++ Assignment_day14_disconnected/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Assignment_day14_disconnected
+{
+    internal class EmpDAL {
+
+        private static string _connectionString;
+        SqlDataAdapter dataAdapter;
+        DataSet dataset=new DataSet();
+        SqlConnection connection;
+        public EmpDAL(IConfiguration iconfiguration)
+        {
+            _connectionString = iconfiguration.GetConnectionString("Default");
+            connection = new SqlConnection(_connectionString);
+        }
+
+        public DataTable GetList()
+        {
+            dataAdapter = new SqlDataAdapter("select * from Employee2", connection);
+            dataAdapter.FillSchema(dataset, SchemaType.Source, "Employee2");
+            dataAdapter.Fill(dataset, "Employee2");
+            Console.WriteLine(dataset.GetXml());
+
+            dataset.Tables[0].TableName = "Employee2";
+            DataTable dt = dataset.Tables["Employee2"];
+           
+            return dt;
+        }
+
+       public void addindataset()
+        {
+            DataRow drCurrent = dataset.Tables["Employee2"].NewRow();
+            drCurrent["Id"] = 9;
+            drCurrent["Name"] = "Akshita";
+            drCurrent["Salary"] = 7000000;
+
+            dataset.Tables["Employee2"].Rows.Add(drCurrent);
+            Console.WriteLine("Add was successful, Click any key to continue!!");
+
+            SqlCommandBuilder co = new SqlCommandBuilder(dataAdapter);
+            Console.WriteLine(co.GetInsertCommand().CommandText);
+            dataAdapter.Update(dataset, "Employee2");
+
+        }
+
+        public void deletebyid(int id)
+        {
+            DataRow dd = dataset.Tables[0].Rows.Find(id);
+            dd.Delete();
+            SqlCommandBuilder co = new SqlCommandBuilder(dataAdapter);
+            dataAdapter.Update(dataset, "Employee2");
+            Console.WriteLine(co.GetDeleteCommand().CommandText);
+        }
+    }
+}
---
+++ Assignment_day14_disconnected/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace Assignment_day14_disconnected
+{
+    internal class Program
+    {
+        private static IConfiguration _iconfiguration;
+
+        static void GetAppSettingsFile()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("Pro1.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+        }
+
+        static void PrintProduct()
+        {
+            var empdal = new EmpDAL(_iconfiguration);
+            var dt = empdal.GetList();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (DataColumn col in dt.Columns)
+                    Console.Write(row[col] + " ");
+                Console.WriteLine("----------------------------------");
+
+            }
+
+           // empdal.addindataset();
+           //empdal.deletebyid(4);
+
+        }
+            static void Main(string[] args)
+        {
+            GetAppSettingsFile();
+            PrintProduct();
+
+            
+        }
+    }
+}
